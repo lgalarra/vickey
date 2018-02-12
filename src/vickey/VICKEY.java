b1ef51153e0e0d2ea6339dd143a9ffa4fe908d47
luis galarraga del prado <lgalarra@tobi.irisa.fr>
2018-02-12 18:53:50 +0100

    Addition of SAKEY as library

---
+++ src/vickey/VICKEY.java
@@ -0,0 +1,13 @@
+package vickey;
+
+import java.io.IOException;
+
+import sakey.SAKeyAlmostKeysOneFileOneN;
+
+public class VICKEY {
+
+	public static void main(String[] args) throws IOException {
+		SAKeyAlmostKeysOneFileOneN.main(args);
+	}
+
+}
